7009720dc510d48a5063b6d1162685f195ec035b
Melanie Weynants <45990429+melwey@users.noreply.github.com>
2023-08-07 19:17:14 +0200

    update era from back_extention to main and extend to 2022

--- scripts/Rechunk_Data.jl
+++ scripts/Rechunk_Data.jl
@@ -23,7 +23,7 @@ using Statistics: mean
 end
 
 @everywhere function era5varcube(varname;ext="an")
-    onenc = Cube("/Net/Groups/data_BGC/era5/e1/0d25_hourly/$varname/1950/$(varname).hh.$(ext).era5_back_extension.01.1950.nc")
+    onenc = Cube("/Net/Groups/data_BGC/era5/e1/0d25_hourly/$varname/1950/$(varname).hh.$(ext).era5.01.1950.nc")
     lonax = onenc.longitude
     latax = onenc.latitude
     T_CF = typeof(onenc.data)
@@ -36,10 +36,10 @@ end
     settings = (;
         varname,lonax, latax, T_CF, T_NC, props, cfargs, spatsize
     )
-    allars = [diskarfromyearmonth(settings,y,m) for m in 1:12, y in 1950:2021]
+    allars = [diskarfromyearmonth(settings,y,m) for m in 1:12, y in 1950:2022]
 
     onear = ConcatDiskArray(reshape(allars,1,1,length(allars)))
-    timax = RangeAxis("time",DateTime(1950,1,1):Hour(1):DateTime(2021,12,31,23,0,0))
+    timax = RangeAxis("time",DateTime(1950,1,1):Hour(1):DateTime(2022,12,31,23,0,0))
     YAXArray([lonax,latax,timax],onear,props)
 end
 
@@ -50,7 +50,7 @@ end
     m2 = @sprintf("%02d",m)
     fshort = filter(endswith("$m2.$y.nc"),filelist)
     if length(fshort)>1
-        filter!(contains("back_extension"),fshort)
+        filter!(!contains("back_extension"),fshort)
     end
     @assert length(fshort)==1
     fnow = joinpath("/Net/Groups/data_BGC/era5/e1/0d25_hourly/$(settings.varname)/$(y)/",fshort[1])
@@ -77,12 +77,20 @@ map(to_run) do (varname,outvarname,ext,aggfun)
         a2.a.parents[1]=a2.a.parents[3]
     end
 
-    c2 = YAXArray([c.longitude,c.latitude,RangeAxis("time",Date(1950,1,1):Day(1):Date(2021,12,31))],a2,c.properties)
+    # add aggregation function to properties
+    c.properties["aggfun"] = string(aggfun)
+
+    # set time axis until 2029 for later updates, but keep empty
+    c2 = YAXArray([c.longitude,c.latitude,RangeAxis("time",Date(1950,1,1):Day(1):Date(2029,12,31))],a2,c.properties)
 
     nt = (Symbol(outvarname)=>c2,)
     ds = Dataset(;nt...)
     ds = setchunks(ds,(lon=60,lat=60,time=5844))
+    # data are Float64, so 1 chunk (for one variable) would be around 64*60*60*5844/8 = 168 MB
 
-    savedataset(ds, path="/scratch/fgans/XAIDAERA5Cube.zarr",append=true, max_cache=3e8,writefac=0.1)
+    # writefac: read/write speed factor. Estimate of time ratio between reading input and writing output.
+    # Usually reading input is faster than writing output, hence default value is 4.0
+    # Here reading is much slower than writing because we aggregate on the fly the hourly data to daily, therefore value is set to 0.1.
+    savedataset(ds, path="/scratch/mweynants/ERA5CubeDE.zarr",append=true, max_cache=3e8,writefac=0.1)
     nothing
 end
