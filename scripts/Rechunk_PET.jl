564a98a7717d1572c0e02859c603dc7fe951d966
Melanie Weynants <45990429+melwey@users.noreply.github.com>
2023-09-21 11:54:36 +0200

    rename dimensions in PET cubes

--- scripts/Rechunk_PET.jl
+++ scripts/Rechunk_PET.jl
@@ -17,12 +17,15 @@ using YAXArrayBase
 
 outpath = "/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/ERA5Cube.zarr"
 
+# need to find a way to 
+# - rename axis Time to time
+# - rename cubes from :layer to :pet
+map(1950:2022) do yr
+    mv("/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/layer", "/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/pet")
+    mv("/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/Time", "/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/time")
+end
 ds = YAXArrays.Datasets.open_mfdataset("/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/*.zarr")
 ds = setchunks(ds,(lon=60,lat=60,time=5844))
-# need to find a way to 
-# - rename cubes from :layer to :pet
-# - rename axis Time to time
-renameaxis!(ds, "Time" => "time")
 
 savedataset(ds, path=outpath,append=true,max_cache=1e9)
 println("Done!")
