21822479425f5cd9c09b1df39e1d30e0795c6f8c
Melanie Weynants <45990429+melwey@users.noreply.github.com>
2023-09-26 16:44:29 +0200

    postpone renaming of time axis

--- scripts/Rechunk_PET.jl
+++ scripts/Rechunk_PET.jl
@@ -22,10 +22,32 @@ outpath = "/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/ERA5Cube.zarr"
 # - rename cubes from :layer to :pet
 map(1950:2022) do yr
     mv("/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/layer", "/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/pet")
-    mv("/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/Time", "/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/time")
+    # mv("/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/Time", "/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/time")
+    # mv("/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/time", "/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr/Time")
 end
 ds = YAXArrays.Datasets.open_mfdataset("/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/*.zarr")
+# # ERROR: Dimension ranges overlap
+# # check cubes individually and identify which ones are problematic
+# for yr in 1950:2022
+#     println(yr)
+#     @show ds = open_dataset("/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/PET/$yr.zarr")
+# end
+## problem comes from the definition of the time axis. it wants Time and doesn't recognise time. So revert rename Time axis to time.
+
+## Renaming ds time axis
+# renameaxis!(ds, :Time => :time)
+# ERROR: MethodError: no method matching renameaxis!(::Dataset, ::Pair{Symbol, Symbol})
+# Closest candidates are:
+# renameaxis!(::YAXArray, ::Pair)
+## Do it at a still later stage
 ds = setchunks(ds,(lon=60,lat=60,time=5844))
 
 savedataset(ds, path=outpath,append=true,max_cache=1e9)
 println("Done!")
+
+# rm -rf /Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/ERA5Cube.zarr/Time
+# rm -rf /Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/ERA5Cube.zarr/layer
+
+
+
+# consolidate metadata
