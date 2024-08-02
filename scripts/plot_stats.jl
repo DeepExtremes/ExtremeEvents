44a134897056dc22e2108043fa2603ccbe4dd452
Melanie Weynants <45990429+melwey@users.noreply.github.com>
2024-08-02 22:21:28 +0200

    add hexbin plot

--- scripts/plot_stats.jl
+++ scripts/plot_stats.jl
@@ -19,7 +19,8 @@ end
 
 trial = "ranked_pot0.01_ne0.1_cmp_S1_T3_2010_2022"
 landonly = "_landonly"
-events = CSV.read(path * "EventStats_" * trial * landonly * ".csv", DataFrame);
+# events = CSV.read(path * "EventStats_" * trial * landonly * ".csv", DataFrame);
+ev = CSV.read(path*"MergedEventStats"*landonly*".csv", DataFrame)
 # look for intersection between spatial and temporal range of events from the table or directly in the labelcube
 # labelpath = path * "labelcube_$trial.zarr"
 labelpath = "/Net/Groups/BGI/work_1/scratch/fgans/DeepExtremes/MergeLabelCubes/mergedlabels.zarr"
@@ -34,7 +35,7 @@ lsm = Cube("/Net/Groups/data_BGC/era5/e1/0d25_static/lsm.1440.721.static.nc")[
 # Plots.heatmap(lsm.data'[end:-1:1,:])
 # lsmask = lsm.data .> 0.5;
 
-f, ax, h = hist(log10.(events.volume[1:1000]), 
+f, ax, h = hist(log10.(ev.volume[1:1000]), 
     color = :tomato,
     label = "land only",
     figure = (; size = (400, 400)),
@@ -42,26 +43,68 @@ f, ax, h = hist(log10.(events.volume[1:1000]),
     )
 save(path * "/fig/events_1000_volume_$trial$landonly.png", f)
 
+function myhexbin(ev::DataFrame)
+    # 2d histogram (hexbin)
+    
+    f = Figure(size = (800,400))
+    ax1 =  Axis(f[1,1],
+        xlabel = "log10 (Area)",
+        ylabel = "duration (days)",)
+    hb = hexbin!(ax1, log10.(ev.area), ev.d, cellsize = (0.1, 1.2),
+        colorscale=log10)
+    Colorbar(f[2,1], hb,
+        label = "Number of labelled events",
+        vertical = false,
+        # height = Relative(0.5)
+    )
 
-# days = parse.(Int, replace.(events.duration, r" day(s)?" => "")); days[1:10]
-# p1 = Plots.scatter(log10.(events.area[1001:end]), days[1001:end],
-#     label = "smaller events",
-#     xlabel = "log10 (Area)",
-#     ylabel = "duration",
-#     color = :grey60,
-#     markershape = :circle,
-#     markersize = 2,
-#     markerstrokecolor = :grey60,
-#     )
-# Plots.scatter!(log10.(events.area[1:1000]), days[1:1000],
-#     label="1000 largest events in Volume", 
-#     color = :grey30,
-#     markershape = :circle,
-#     markersize = 2,
-#     markerstrokecolor = :grey30,)
-# savefig(p1,path * "fig/events_area_vs_duration_$trial$landonly.png")
+    # volume over time
+    ax2 =  Axis(f[1,2],
+        xlabel = "Year of event onset",
+        ylabel = "log10(Volume)",)
+    hb = hexbin!(ax2, year.(ev.start_time), log10.(ev.volume), cellsize = (1, 0.15),
+        colorscale=log10,
+        )
+    Colorbar(f[2, 2], hb,
+        label = "Number of labelled events",
+        vertical = false,
+        # height = Relative(0.5)
+    )
+    return f
+end
+f = myhexbin(ev)
+save(path * "fig/events_stats_$trial$(landonly)_1950.png", f,)
 
-# Plots.scatter(log10.(events.area[1:1000]), days[1:1000],color=:red,label="1000 largest events in Volume")
+# filter from 1970
+fev = ev |>
+    (df -> transform(df, :duration => (x -> parse.(Int, replace.(x, r" day(s)?" => ""))) => :d)) |>
+    (df -> filter([:start_time, :d] => (t, d) -> t .>= DateTime(1970,1,1) .&& d .> 2, df))
+f1 = myhexbin(fev)
+save(path * "fig/events_stats_$trial$(landonly)_1970.png", f1,)
+# extract stats
+print(Statistics.quantile(fev.d, [0.05, 0.25, 0.5, 0.75, 0.95]))
+# [4.0, 4.0, 4.0, 5.0, 9.0]
+print(Statistics.quantile(fev.area, [0.05, 0.25, 0.5, 0.75, 0.95]))
+# [0.4146932363510132, 0.8429072499275208, 0.9996573328971863, 2.985333263874054, 24.471434984888276]
+print(Statistics.quantile(fev.volume, [0.05, 0.25, 0.5, 0.75, 0.95]))
+# [1.7510369420051575, 3.587491035461426, 5.430251181125641, 15.98317289352417, 164.24727816581685]
+
+# volume quantiles by year
+gdf = fev |>
+    (df -> transform(df, :start_time => (x -> year.(x)) => :yr)) |>
+    (df -> DataFrames.groupby(df, :yr)) |>
+    (gdf -> combine(gdf) do sdf
+        (q05, q25, q50, q75, q95) = Statistics.quantile(sdf.volume, [0.05, 0.25, 0.5, 0.75, 0.95])
+        DataFrame(yr = sdf.yr[1], q05 = q05, q25 = q25, q50 = q50, q75 = q75, q95 = q95)
+    end)
+
+fig = Figure();
+ax = Axis(fig[1, 1], yscale = log10)
+for q in [:q05, :q25, :q50, :q75, :q95]
+    lines!(ax, gdf[:,:yr], gdf[:, q])
+end
+
+fig
 
 # test hm
 ls = Cube("/Net/Groups/data_BGC/era5/e1/0d25_static/lsm.1440.721.static.nc")
