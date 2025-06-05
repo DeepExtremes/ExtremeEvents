d9430e8604d5d34e33408db70023cdecd59df4fb
Melanie Weynants <45990429+melwey@users.noreply.github.com>
2025-06-05 11:38:13 +0200

    compare PEI_x at ICOS sites with SPEI_x from Pohl (EOBS) and with SPEI_x from Liu (ERA5)

--- SlurmScripts/consolidate_metadata.sh
+++ SlurmScripts/consolidate_metadata.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 # PATH2CUBE="/Net/Groups/BGI/scratch/mweynants/DeepExtremes/v3/ERA5Cube_2023.zarr"
-PATH2CUBE=/Net/Groups/BGI/work_2/scratch/mweynants/Dheed_v4/ERA5Cube.zarr
+# PATH2CUBE=/Net/Groups/BGI/work_2/scratch/mweynants/Dheed_v4/ERA5Cube.zarr
+# PATH2CUBE=/Net/Groups/BGI/work_2/scratch/mweynants/Dheed_v4/EventCube_ranked_pot0.01_ne0.1.zarr/
+PATH2CUBE=/Net/Groups/BGI/work_2/scratch/mweynants/Dheed_v4/mergedlabels_ranked_pot0.01_ne0.1_cmp_S1_T3_1950_2023.zarr/
 python -c "import zarr; g = zarr.open_group(\"${PATH2CUBE}\"); zarr.consolidate_metadata(g.store)"
 
 # # manually edit .zmetadata: modify per dimensions :Time => :time
